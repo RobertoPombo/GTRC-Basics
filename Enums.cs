6829e2aeac6ea4ca7c2972a6577517b4294ab753
RobertoPombo <captn.sponge2@web.de>
2023-12-10 21:06:57 +0100

    Projektdateien hinzufügen.

---
+++ Enums.cs
@@ -0,0 +1,97 @@
+﻿namespace GTRC_Basics
+{
+    public enum TimeTypeEnum
+    {
+        Miliseconds = 0,
+        Seconds = 1,
+        Minutes = 2,
+        Hours = 3,
+        Days = 4,
+        Weeks = 5,
+        Months = 6,
+        Years = 7
+    }
+
+    public enum SessionTypeEnum
+    {
+        Practice = 0,
+        Qualifying = 1,
+        Race = 2
+    }
+
+    public enum ServerTypeEnum
+    {
+        PreQuali = 0,
+        Practice = 1,
+        Event = 2
+    }
+
+    public enum EntrylistTypeEnum
+    {
+        None = 0,
+        RaceControl = 1,
+        AllDrivers = 2,
+        Season = 3
+    }
+
+    public enum IncidentsStatusEnum
+    {
+        Open = 0,
+        DoneLive = 1,
+        DonePostRace = 2,
+        Discarded = 3
+    }
+
+    public enum ReportReasonEnum
+    {
+        ManualReport = 0,
+        Collision = 1,
+        ReturnToGarage = 2,
+        WrongStartingPos = 3,
+        OvertakeQuali = 4,
+        OvertakeOffTrack = 5,
+        DriverReport = 6
+    }
+
+    public enum IncidentPropCategoryEnum
+    {
+        OriginalReport = 0,
+        Report = 1,
+        Description = 2,
+        Decision = 3,
+        Status = 4
+    }
+
+    public enum FormationLapTypeEnum
+    {
+        Manual = 0,
+        ManualShort = 1,
+        Controlled = 2,
+        ControlledShort = 3,
+        Limiter = 4,
+        LimiterShort = 5
+    }
+
+    public enum DayOfWeekendEnum
+    {
+        Friday = 1,
+        Saturday = 2,
+        Sunday = 3
+    }
+
+    public enum RtgEnum
+    {
+        NoRTG = 0,
+        Pitlane = 1,
+        PitEntry = 2,
+        PitExit = 3,
+        Track = 4,
+    }
+
+    public enum SessionStateEnum
+    {
+        DNS = 0,
+        Running = 1,
+        Finished = 2,
+    }
+}
---
+++ GlobalValues.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace GTRC_Basics
+{
+    public delegate void Notify();
+
+    public static class GlobalValues
+    {
+        public static readonly int NoID = -1;
+        public static readonly int ID0 = 1;
+        public static readonly DateTime DateTimeMinValue = DateTime.MinValue.AddYears(1800);
+        public static readonly DateTime DateTimeMaxValue = DateTime.MaxValue.AddDays(-1);
+
+        private static string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        private static string currentLogText = " ";
+
+        public static string BaseDirectory { get { return baseDirectory; } set { baseDirectory = value; } }
+        public static string DataDirectory { get { return baseDirectory + "data\\"; } }
+        public static string CurrentLogText { get { return currentLogText; } set { currentLogText = value; OnNewLogText(); } }
+        public static Brush StateOff { get { return ThemeColors.color0; } }
+        public static Brush StateOn { get { return ThemeColors.color3; } }
+        public static Brush StateWait { get { return ThemeColors.color6; } }
+        public static Brush StateRun { get { return ThemeColors.color5; } }
+        public static Brush StateRunWait { get { return ThemeColors.color4; } }
+
+        public static event Notify? NewLogText;
+        public static void OnNewLogText() { NewLogText?.Invoke(); }
+    }
+}
