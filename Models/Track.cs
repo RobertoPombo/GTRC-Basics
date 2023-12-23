29663cd691d007bed2f8f35fbe26ed76efcb7dac
RobertoPombo <captn.sponge2@web.de>
2023-12-23 12:17:50 +0100

    Renamed properties for Car, Track models

--- Models/Track.cs
+++ Models/Track.cs
@@ -5,13 +5,13 @@ namespace GTRC_Basics.Models
     public class Track : BaseModel
     {
         public static readonly string DefaultAccTrackID = "TrackID";
-        static Track() { UniqProps = [[typeof(Track).GetProperty(nameof(AccTrackID))!]]; }
+        static Track() { UniqProps = [[typeof(Track).GetProperty(nameof(AccTrackId))!]]; }
 
-        public string AccTrackID { get; set; } = DefaultAccTrackID;
+        public string AccTrackId { get; set; } = DefaultAccTrackID;
         public string Name { get; set; } = "";
         public int PitBoxesCount { get; set; } = 0;
         public int ServerSlotsCount { get; set; } = 0;
         public int AccTimePenDT { get; set; } = 30;
-        public string Name_GTRC { get; set; } = "";
+        public string NameGtrc { get; set; } = "";
     }
 }
