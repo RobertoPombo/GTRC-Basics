1556ca45f86acda92959cdcbec049d30011235a3
RobertoPombo <captn.sponge2@web.de>
2024-08-05 01:07:03 +0200

    Renamed variables of Table StintAnalysisMethod.

--- Models/DTOs/Individual/StintAnalysisMethod.cs
+++ Models/DTOs/Individual/StintAnalysisMethod.cs
@@ -19,12 +19,12 @@ namespace GTRC_Basics.Models.DTOs
         public string? Name { get; set; }
         public bool? UseResultsFromSimulation { get; set; }
         public ushort? LapRange { get; set; }
-        public ushort? MaxInvalidLapsInRange { get; set; }
-        public uint? MaxTimeDeltaInRangeMs { get; set; }
-        public ushort? ConsecutiveValidLapsMin { get; set; }
+        public ushort? MaxLapsInvalid { get; set; }
+        public ushort? MaxTimeDeltaPercent { get; set; }
+        public ushort? MinLapsValidConsecutive { get; set; }
         public ushort? MinLapsCount { get; set; }
         public ushort? MaxLapsCount { get; set; }
-        public bool? ForceLapsCountConsecutive { get; set; }
+        public bool? ForceLapsConsecutive { get; set; }
         public bool? CombineSessions { get; set; }
         public ushort? LapsCountCombined { get; set; }
     }
--- Models/Individual/StintAnalysisMethod.cs
+++ Models/Individual/StintAnalysisMethod.cs
@@ -5,6 +5,7 @@ namespace GTRC_Basics.Models
     public class StintAnalysisMethod : IBaseModel
     {
         public static readonly string DefaultName = "Stint Analysis Method #1";
+        public static readonly ushort MinMaxTimeDeltaPercent = 100;
 
         public override string ToString() { return Name; }
 
@@ -12,12 +13,12 @@ namespace GTRC_Basics.Models
         public string Name { get; set; } = DefaultName;
         public bool UseResultsFromSimulation { get; set; } = true;
         public ushort LapRange { get; set; } = ushort.MaxValue;
-        public ushort MaxInvalidLapsInRange { get; set; } = ushort.MaxValue;
-        public uint MaxTimeDeltaInRangeMs { get; set; } = uint.MaxValue;
-        public ushort ConsecutiveValidLapsMin { get; set; } = ushort.MinValue;
+        public ushort MaxLapsInvalid { get; set; } = ushort.MaxValue;
+        public ushort MaxTimeDeltaPercent { get; set; } = ushort.MaxValue;
+        public ushort MinLapsValidConsecutive { get; set; } = ushort.MinValue;
         public ushort MinLapsCount { get; set; } = ushort.MinValue;
         public ushort MaxLapsCount { get; set; } = ushort.MaxValue;
-        public bool ForceLapsCountConsecutive { get; set; } = false;
+        public bool ForceLapsConsecutive { get; set; } = false;
         public bool CombineSessions { get; set; } = false;
         public ushort LapsCountCombined { get; set; } = ushort.MinValue;
     }
